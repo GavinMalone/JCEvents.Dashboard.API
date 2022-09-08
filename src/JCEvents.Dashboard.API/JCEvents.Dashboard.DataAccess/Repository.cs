2f592548a6383b933ec1343a0d446a1813fa8b9e
gavin.malone <Gavin.malone01@gmail.com>
2022-09-08 22:14:42 +0100

    delete and spec functionality

--- src/JCEvents.Dashboard.API/JCEvents.Dashboard.DataAccess/Repository.cs
+++ src/JCEvents.Dashboard.API/JCEvents.Dashboard.DataAccess/Repository.cs
@@ -10,6 +10,30 @@ namespace JCEvents.Dashboard.DataAccess
     {
         public IEnumerable<Stock> GetAllStockItems() => ExecuteListQuery<Stock>("dbo.FindAllStock");
 
+        public IEnumerable<AssignedStock> RetrieveAssignedStockItems(string jobQuotation)
+        {
+            using (var connection = new SqlConnection(ConnectionString.Value))
+            {
+                connection.Open();
+
+                var parameters = new { jobQuotation = jobQuotation };
+
+                return connection.Query<AssignedStock>("dbo.RetrieveStockAssignedToJob", parameters, commandType: CommandType.StoredProcedure).ToList();
+            }
+        }
+
+        public void RemoveAssignedStockItems(int stockId)
+        {
+            using (var connection = new SqlConnection(ConnectionString.Value))
+            {
+                connection.Open();
+
+                var parameters = new { stockItemId = stockId };
+
+                connection.Execute("dbo.RemoveAssignedStockItem", parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
         public IEnumerable<Job> GetJobs() => ExecuteListQuery<Job>("dbo.FindAllJobs");
 
         public void CreateJob(Job job) 
@@ -48,6 +72,18 @@ namespace JCEvents.Dashboard.DataAccess
         }
 
         public Job FindJob(string identifier) => ExecuteQueryWithParameters<Job>("dbo.FindJob", new { quotation = identifier });
+
+        public void DeleteJob(string jobQuotation) 
+        {
+            using (var connection = new SqlConnection(ConnectionString.Value))
+            {
+                connection.Open();
+
+                var parameters = new { jobQuotationNumber = jobQuotation };
+
+                connection.Execute("dbo.DeleteJob", parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
 
         private T ExecuteQueryWithParameters<T>(string storedProcedureName, object parameters)
         {
